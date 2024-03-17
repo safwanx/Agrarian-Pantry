1c2f0625f93d851eae23afa0ec8493ff1d811cfb
Safwanx <srianh2340@gmail.com>
2024-03-17 16:58:08 +0300

    .

--- misc/pantry_database.sql
+++ misc/pantry_database.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL, 
     password VARCHAR(255) NOT NULL,
     role ENUM('Customer', 'Seller') NOT NULL
 );
