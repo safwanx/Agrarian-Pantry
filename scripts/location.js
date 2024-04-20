934831d70512c315f18a9da986f8e78ffa41bfad
Safwanx <srianh2340@gmail.com>
2024-04-20 11:53:21 +0300

    starting with the location, just basic code for now

---
+++ scripts/location.js
@@ -0,0 +1,19 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                document.getElementById("address").value = results[0].formatted_address;
+            }
+        }
+    });
+}
